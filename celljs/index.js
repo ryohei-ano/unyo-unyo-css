d8c99351be32b7fc72170b2c8e027a3975d17f9a
hideaki goto <gotop7dfe@gmail.com>
2018-06-12 21:24:48 +0900

    first commit

--- celljs/index.js
+++ celljs/index.js
@@ -2,7 +2,7 @@ var MAX = 10;
 var RADIUS = 100;
 var FPS = 60;
 var CENTER = {x:200, y:200};
-var CENTER2 = {x:100, y:100};
+var CENTER2 = {x:172, y:172};
 var canvas, ctx, timer, point;
 var point1;
 var stats;
@@ -37,19 +37,15 @@ var Point = function(c, r, rota)
 
         if(this.rota > 360){ this.rota -= 360; };
     }  
-}
 
+}
 function initialize(){
     var rota = 360 / MAX;
     var i;
-    var x;
     for(i = 0; i < MAX; i++)
     {
         point[i] = new Point(CENTER, RADIUS, rota * i);
-    }
-    for(x = 0; x < MAX; x++)
-    {
-        point1[x] = new Point(CENTER2, RADIUS, rota * x);
+        point1[i] = new Point(CENTER2, RADIUS, rota * i);
     }
 }
 
@@ -59,17 +55,16 @@ function update(){
     {
         point[i].update();
     }
-    for(var x = 0; x < MAX; x++)
+    for(var i = 0; i < MAX; i++)
     {
-        point1[x].update();
+        point1[i].update();
     }
 
-    draw1();
     draw();
+    draw1();
 }
 
 function draw1(){  
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     if(mouseFlag){
         ctx.fiiStyle = "rgba(0, 255, 0, 0.5)";
     }else{
@@ -84,37 +79,38 @@ function draw1(){
 
     ctx.moveTo(xc1, yc1);
 
-    for(var x = 0; x < MAX - 1; x++){
+    for(var i = 0; i < MAX - 1; i++){
 
-        var xc = (point1[x].x + point1[x + 1].x + 10) / 2;
-        var yc = (point1[x].y + point1[x + 1].y + 10) / 2;
+        var xc = (point1[i].x + point1[i + 1].x) / 2;
+        var yc = (point1[i].y + point1[i + 1].y) / 2;
 
-        ctx.quadraticCurveTo(point1[x].x, point1[x].y, xc, yc)
+        ctx.quadraticCurveTo(point1[i].x, point1[i].y, xc, yc)
     }
 
-    ctx.quadraticCurveTo(point1[x].x, point1[x].y, xc1, yc1);
+    ctx.quadraticCurveTo(point1[i].x, point1[i].y, xc1, yc1);
 
     ctx.closePath();
 
     if(mouseFlag){
-        ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+        ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
         ctx.fill();
     }else{
         ctx.stroke();
     }
-console.log("draw1")
 
     if(mouseFlag){ return; };
 
-    for(x = 0; x < MAX; x++){
+    for(i = 0; i < MAX; i++){
         ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
         ctx.beginPath();
-        ctx.arc(point1[x].x, point1[x].y, 2, 0, Math.PI * 2, false);
+        ctx.arc(point1[i].x, point1[i].y, 2, 0, Math.PI * 2, false);
         ctx.closePath();
         ctx.fill();
     }
 }
+
 function draw(){  
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     if(mouseFlag){
         ctx.fiiStyle = "rgba(0, 255, 0, 0.5)";
     }else{
@@ -148,7 +144,6 @@ function draw(){
         ctx.stroke();
     }
 
-console.log("draw")
     if(mouseFlag){ return; };
 
     for(i = 0; i < MAX; i++){
